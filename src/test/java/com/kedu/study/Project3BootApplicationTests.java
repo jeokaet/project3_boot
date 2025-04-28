155e022766bad0463fc766ed1fabc02f0c0a6066
woong <woong>
2025-04-28 18:16:48 +0900

    처음

---
+++ src/test/java/com/kedu/study/Project3BootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kedu.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project3BootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
