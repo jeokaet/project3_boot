127f3388df43ac7e584e0776d403b6317d4afe03
jeokaet <whgnldud0325@naver.com>
2025-05-12 09:32:44 +0900

    jeokaet / gcp 완!

--- src/main/java/com/kedu/home/config/GCSConfig.java
+++ src/main/java/com/kedu/home/config/GCSConfig.java
@@ -1,30 +1,30 @@
-//package com.kedu.home.config;
-//
-//import java.io.IOException;
-//import java.io.InputStream;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.io.ClassPathResource;
-//
-//import com.google.auth.oauth2.GoogleCredentials;
-//import com.google.cloud.storage.Storage;
-//import com.google.cloud.storage.StorageOptions;
-//
-//
-//@Configuration
-//public class GCSConfig {
-//	
-//	@Value("${gcs.key.location}")
-//	private String gcsKeyLocation;
-//	
-//	@Bean
-//	public Storage getStorage() throws IOException{
-//		try(InputStream is = new ClassPathResource(gcsKeyLocation).getInputStream();){
-//			GoogleCredentials key = GoogleCredentials.fromStream(is);
-//	         return StorageOptions.newBuilder().setCredentials(key).build().getService();
-//		}
-//
-//	}
-//}
+package com.kedu.home.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+
+
+@Configuration
+public class GCSConfig {
+	
+	@Value("${gcs.key.location}")
+	private String gcsKeyLocation;
+	
+	@Bean
+	public Storage getStorage() throws IOException{
+		try(InputStream is = new ClassPathResource(gcsKeyLocation).getInputStream();){
+			GoogleCredentials key = GoogleCredentials.fromStream(is);
+	         return StorageOptions.newBuilder().setCredentials(key).build().getService();
+		}
+
+	}
+}
