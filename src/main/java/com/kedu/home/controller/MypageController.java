3f78fe499adef9dec3a778647a4fc1bb7868c6b2
woong <woong>
2025-05-13 17:36:13 +0900

    crud

--- src/main/java/com/kedu/home/controller/MypageController.java
+++ src/main/java/com/kedu/home/controller/MypageController.java
@@ -2,10 +2,15 @@ package com.kedu.home.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kedu.home.dto.MemberDTO;
 import com.kedu.home.services.MypageService;
 
 @RestController
@@ -16,9 +21,24 @@ public class MypageController {
 	private MypageService mServ;
 	
 	@GetMapping("/getMembers")
-	public ResponseEntity<String> getMembers(){
+	public ResponseEntity<MemberDTO> getMembers(@RequestParam String loginId){
+	
+		MemberDTO result = mServ.getMembers(loginId);
 		
-//		mServ.getMembers();
+		return ResponseEntity.ok(result);
+	}
+	
+	@PostMapping("/updateMember")
+	public ResponseEntity<MemberDTO> updateMember(@RequestBody MemberDTO request){
+		mServ.updateMember(request);
+		
+		return ResponseEntity.ok(null);
+	}
+	
+	@DeleteMapping("/deleteMember")
+	public ResponseEntity<String> deleteMember(@RequestParam String loginId){
+		mServ. deleteMember(loginId);
+		
 		return ResponseEntity.ok(null);
 	}
 	
--- src/main/java/com/kedu/home/services/MypageService.java
+++ src/main/java/com/kedu/home/services/MypageService.java
@@ -1,5 +1,26 @@
 package com.kedu.home.services;
 
-public class MypageService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.kedu.home.dao.MypageDAO;
+import com.kedu.home.dto.MemberDTO;
+
+@Service
+public class MypageService {
+	
+	@Autowired
+	private MypageDAO mDao;
+	
+	public MemberDTO getMembers(String loginId) {
+		return mDao.getMembers(loginId);
+	}
+
+	public int updateMember(MemberDTO request) {
+		return mDao.updateMember(request);
+	}
+	
+	public int  deleteMember(String loginId) {
+		return mDao. deleteMember(loginId);
+	}
 }
